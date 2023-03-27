ebca0a040e92e3597aff5861698a1999ca18a078
Favstvs <54644759+Favstvs@users.noreply.github.com>
2023-03-27 15:50:58 +0000

    Update yahooscrap.py

--- yahooscrap.py
+++ yahooscrap.py
@@ -1,3 +1,24 @@
+'''
+PROBLEMA: con pandas.DataFrame() stampa le prime 5 rows
+e poi salta dirtettamente alla 245
+
+                   Names     Prices    Changes %Changes MarketCap    Volume   Supplys
+0            Bitcoin USD  26,926.12    -905.30   -3.25%  520.476B    16.31B    19.33M
+1           Ethereum USD   1,709.44     -58.42   -3.30%  209.191B    7.357B  122.374M
+2             Tether USD     1.0000    -0.0007   -0.07%   79.181B   25.778B    79.18B
+3                BNB USD     315.83     -11.43   -3.49%   49.867B  491.069M  157.889M
+4           USD Coin USD   0.999511  -0.000294   -0.03%   33.768B    3.854B   33.785B
+..                   ...        ...        ...      ...       ...       ...       ...
+245        T-mac DAO USD     5.0286    +0.2064   +4.28%    5.029B   212,834        1B
+246  Wrapped Bitcoin USD  26,954.66    -906.42   -3.25%     4.05B  127.609M   150,250
+247        Chainlink USD     6.8675    -0.3538   -4.90%    3.551B  202.028M    517.1M
+248     UNUS SED LEO USD     3.3762    -0.0793   -2.29%    3.221B    1.304M  953.954M
+249           Cosmos USD      10.76      -0.57   -5.03%    3.082B   97.389M   286.37M
+
+[250 rows x 7 columns]
+
+'''
+
 import requests 
 from bs4 import BeautifulSoup 
 import pandas as pd 
@@ -35,6 +56,7 @@ for i in range(0,10):
 
 for i in range(0, 10):
  print(names[i], prices[i], changes[i], percentChanges[i], marketCaps[i], totalVolumes[i], circulatingSupplys[i])
-
+'''
 df=pd.DataFrame({"Names":names, "Prices":prices, "Changes":changes, "%Changes":percentChanges, "MarketCap":marketCaps, "Volume":totalVolumes, "Supplys":circulatingSupplys})
 print(df)
+'''
